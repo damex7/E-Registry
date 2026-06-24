42c8b136adef9ecfe97c7ef48adff916cb98e960
damex7 <abdrahmanabubakar7000@gmail.com>
2026-06-24 15:20:54 +0100

    new

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let left = document.getElementById("left");
 let bars = document.getElementById("bars");
 let cancel = document.getElementById("cancel");
-let cancel1 = document.getElementById("cancel1");
-let newFile = document.getElementById("newFile");
-let addFile = document.getElementById("addFile");
-let cancel2 = document.getElementById("cancel2");
-let newMail = document.getElementById("newMail");
-let addMail = document.getElementById("addMail");
 
 bars.addEventListener('click', ()=>{
     if(!left.classList.contains("show")){
@@ -23,20 +17,48 @@ cancel.addEventListener('click', ()=>{
     }
 });
 
-cancel1.addEventListener('click', ()=>{
-    newFile.classList.add("hide");
-});
-
-addFile.addEventListener('click', ()=>{
-    newFile.classList.remove("hide");
-    newFile.classList.add("show");
-});
-
-addMail.addEventListener('click', ()=>{
-    alert("bring!!!");
-})
+// Add file /////////////////////////////////////
+let cancel1 = document.getElementById("cancel1");
+let newFile = document.getElementById("newFile");
+let addFile = document.getElementById("addFile");
 
 
+if(cancel1 && newFile && addFile){
+
+    cancel1.addEventListener('click', ()=>{
+        newFile.classList.add("hide");
+    });
+
+    addFile.addEventListener('click', ()=>{
+        newFile.classList.remove("hide");
+        newFile.classList.add("show");
+    });
+
+}
+
+// /////////////////////////////////////////////
+
+// Add Mail ///////////////////////////////////////
+let cancel2 = document.getElementById("cancel2");
+let newMail = document.getElementById("newMail");
+let addMail = document.getElementById("addMail");
+
+console.log("Script reached Add Mail section");
+
+if(cancel2 && newMail && addMail){
+
+    cancel2.addEventListener('click', ()=>{
+        newMail.classList.add("hide");
+    });
+
+    addMail.addEventListener('click', ()=>{
+        newMail.classList.remove("hide");
+        newMail.classList.add("show");
+    });
+
+}
+
+//////////////////////////////////////////////////
 // file detail
 let actionsheet = document.getElementById("actionsheet");
 let mhistory = document.getElementById("mhistory");
@@ -46,41 +68,47 @@ let newtable1 = document.getElementById("newtable1");
 let newtable2 = document.getElementById("newtable2");
 let newtable3 = document.getElementById("newtable3");
 
-actionsheet.addEventListener('click', ()=>{
-    if(!actionsheet.classList.contains("show")){
-        newtable1.style.display = "block";
-        newtable2.style.display = "none";
-        newtable3.style.display = "none";
-    }
-    else{
-        newtable1.style.display = "block";
-    }
-});
 
-mhistory.addEventListener('click', ()=>{
-    if(!mhistory.classList.contains("show")){
-        newtable1.style.display = "none";
-        newtable2.style.display = "block";
-        newtable3.style.display = "none";
-    }
-    // else{
-    //     newtable3.style.display = "none";
-    // }
-});
+if(actionsheet && mhistory && maildets){
+    actionsheet.addEventListener('click', ()=>{
+        if(!actionsheet.classList.contains("show")){
+            newtable1.style.display = "block";
+            newtable2.style.display = "none";
+            newtable3.style.display = "none";
+        }
+        else{
+            newtable1.style.display = "block";
+        }
+    });
 
-maildets.addEventListener('click', ()=>{
-    if(!maildets.classList.contains("show")){
-        newtable1.style.display = "none";
-        newtable2.style.display = "none";
-        newtable3.style.display = "block";
+    mhistory.addEventListener('click', ()=>{
+        if(!mhistory.classList.contains("show")){
+            newtable1.style.display = "none";
+            newtable2.style.display = "block";
+            newtable3.style.display = "none";
+        }
+        // else{
+        //     newtable3.style.display = "none";
+        // }
+    });
 
-    }
-})
+    maildets.addEventListener('click', ()=>{
+        if(!maildets.classList.contains("show")){
+            newtable1.style.display = "none";
+            newtable2.style.display = "none";
+            newtable3.style.display = "block";
+
+        }
+    })
+}
+
 
 const img = document.getElementById("profileImg"),
 imgUpload = document.getElementById("imgUpload");
 
-imgUpload.addEventListener("change", ()=> {
-    img.src = URL.createObjectURL(imgUpload.files[0]);
-});
+if(img && imgUpload){
+    imgUpload.addEventListener("change", ()=>{
+        img.src = URL.createObjectURL(imgUpload.files[0]);
+    });
+}
 
